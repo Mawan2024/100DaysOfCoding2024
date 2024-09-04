11a41caf6f115b41479fe000636d3792dfae71a0
Mawan2024 <mwan6703@gmail.com>
2024-09-04 10:27:22 +0800

    Hari 3
    Merampikan println

---
+++ Day3.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args){
+    package 3;
+    System.out.println("Nama saya mawan ");
+		System.out.println("nim D0224018");
+		System.out.println("saya berasal dari mamasa");
+		System.out.println("hobi saya adala badminton");
+    System.out.println("saya berusia 18 tahun");
