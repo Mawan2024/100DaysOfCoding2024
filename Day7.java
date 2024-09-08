0f86c44a2304d80157b63e2d7403fb3dcc5a67b7
Mawan2024 <mwan6703@gmail.com>
2024-09-08 10:11:14 +0800

    Hari 7
    Merampikan coding variable insialisasi

---
+++ Day7.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		String nama =("mawan");
+		String nim =("D0224018");
+		String prodi =("informatika");
+		String alamat =("mamasa");
+		String asal =("smkn 1 sumarorong");
+		int umur  =18;
