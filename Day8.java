dad90583e03f70c8d830b9c58e3d06dd0859f4e2
Mawan2024 <mwan6703@gmail.com>
2024-09-09 20:28:32 +0800

    Day 8
    Merampikan variable update

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		// sebelum di update
+				int umur = 18;
+		System.out.println("umur:" + umur );
+		
+		// sesuda di update 
+		   
+		   umur =85;
+		   System.out.println("umur setela di update:" + umur );
