d2e2059e59b1c151042ce80948609440cb2caef7
Mawan2024 <mwan6703@gmail.com>
2024-09-10 11:55:22 +0800

    Hari 9
    Merampikan variable  costanta

---
+++ Day9.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		double nilaisatu = 5.67;
+		final double PHI_LINGKARAN =3.14;
+		System.out.println("nilai satu: + nilai satu");
+		nilaisatu =7.58;
+		System.out.println("nilai satu: + nilai satu");
