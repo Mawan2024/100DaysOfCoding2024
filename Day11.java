086a689191b9034a8f0184c8e11daf2590f6fc1d
Mawan2024 <mwan6703@gmail.com>
2024-09-12 07:30:12 +0800

    Hari ke 11
    Merampikan type data short

---
+++ Day11.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		short maksimal = -32768;
+		short minimum = 32767;
+		 System.out.println("nilai maksimal dari short adala + maksimal");
+		 System. out.println("nilai minimum dari short adala + maksimum ");
