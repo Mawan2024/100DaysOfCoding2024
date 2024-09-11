b2f2e23bd9041f05889d8866fd9e9f20172f675f
Mawan2024 <mwan6703@gmail.com>
2024-09-11 14:06:23 +0800

    Hari 10
    Merampikan tipe data byte

---
+++ Day10.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		
+		byte minimum =  -128;
+		byte maksimum = 127;
+		 
+		 
+		 System.out.println("nilai minimum:" +minimum);
+		 System.out.println("nilai maksimum:" + maksimum);
+		 byte umur = 28;
+		 byte angka = 25;
+		 System.out.println("umur:" + angka);
+		 
+	}
