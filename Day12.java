fe17e07fd919adf51efc669a9c2e4da91ba757f0
Mawan2024 <mwan6703@gmail.com>
2024-09-13 05:36:23 +0800

    Hari ke 12
    Merampikan type data int

---
+++ Day12.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int maksimal = -2147483648;
+		
+		int minimum = 2147483647;
+		 
+		 System.out.println("nilai maksimal dari int + maksimal");
+		 System.out.println("nilai minimum dari int adala + minimum");
