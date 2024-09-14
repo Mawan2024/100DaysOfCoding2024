19639aa09785d03cbfa594e85efce051fcfb81e2
Mawan2024 <mwan6703@gmail.com>
2024-09-14 15:15:54 +0800

    Hari 13
    Soal evaluasi

---
+++ DAy13.java
@@ -0,0 +1,29 @@
+package mawan;
+
+
+public class Mawan {
+
+    
+    public static void main(String[] args) {
+        int umur =18; 
+         int tinggibadan =159;
+         final String nama ="mawan";
+         System.out.println("sebelum di update");
+         System.out.println("nama:"+ nama);
+         System.out.println("tahun:"+umur+"tahun");
+         System.out.println("tinggi badan :"+tinggibadan+"cm");
+         
+         
+         //stela di update
+           umur =80;
+            tinggibadan =160;
+               System.out.println("setelah di update");
+         System.out.println("nama:"+ nama);
+         System.out.println("tahun:"+umur+"tahun");
+         System.out.println("tinggi badan :"+tinggibadan+"cm");
+         
+        
+         
+    }
+    
+}
