a108898bb49a41f1b34f558a08a07f2b7e124874
Mawan2024 <mwan6703@gmail.com>
2024-09-15 20:56:10 +0800

    Hari 14
    Merampikan tipe data long

---
+++ Day14.java
@@ -0,0 +1,10 @@
+package Day14;
+
+public class Mawan123 {
+    public static void main(String[] args) {
+        long minimal =-6282192999772L;
+        long maximal = 6282346782747L;
+        System.out.println("nilai minimal adalah"+ minimal);
+        System.out.println("nilaI maximal adalah"+ maximal);
+    }
+}
