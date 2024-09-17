151e835724fb59f3276b3664ea3bea40c32909eb
Mawan2024 <mwan6703@gmail.com>
2024-09-17 19:24:22 +0800

    Hari 16
    Merampikan tipe data float

---
+++ Day16.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		float harga = 24.000f;
+		float BB = 25.000f;
+		System.out.println("harga :"+ harga+" rupiah");
+		System.out.println("BB:"+ BB+" kg");
+	}
+}
