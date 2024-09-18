4023f40c275cfd4c67e4166cf608174c2431b102
Mawan2024 <mwan6703@gmail.com>
2024-09-18 20:25:33 +0800

    Hari 17
    Merampikan tipe data char atau karakter

---
+++ Day17.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		char nama = 'm';
+		char jeniskelamin = 'L';
+		 System.out.println("nama adalah: " + nama );
+		 System.out.println("jenis kelamin:"+ jeniskelamin);
+		
+	}
+}
