cb7c3a2fa35c19c54b9747ee7d6ed9452151f10c
Mawan2024 <mwan6703@gmail.com>
2024-09-19 10:56:10 +0800

    Day18.
    Tipe data boolean

--- Day18.java
+++ Day18.java
@@ -1,8 +1,9 @@
 public class Main {
 	public static void main(String[] args) {
-		boolean javaitumuda = true;
-		boolean javaitusulit = false;
-		 System.out.println("apaka java itu muda:" + javaitumuda);
-		 System.out.println("apaka java itu sulit:"+ javaitusulit);
+		boolean berbuat_baik_itu_bagus = true;
+		boolean berbuat_dosa_itu_baik = false;
+		System.out.println("apaka berbuat baik itu bagus:"+ berbuat_baik_itu_bagus );
+		System.out.println("apakah berbuat dosa itu baik:"+ berbuat_dosa_itu_baik);  
 	}
 }
+
