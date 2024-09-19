94b94e0feb04efe85f9595b9b944c3fe7352f8c4
Mawan2024 <mwan6703@gmail.com>
2024-09-19 10:28:03 +0800

    Hari 18
    Merampikan tipe data boolean

---
+++ Day18.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean javaitumuda = true;
+		boolean javaitusulit = false;
+		 System.out.println("apaka java itu muda:" + javaitumuda);
+		 System.out.println("apaka java itu sulit:"+ javaitusulit);
+	}
+}
