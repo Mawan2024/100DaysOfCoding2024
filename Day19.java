6161c6c4972cdb18b33a48f14867e17197b8bcb8
Mawan2024 <mwan6703@gmail.com>
2024-09-20 10:51:04 +0800

    Hari 19
    Merampikan tipe data string

---
+++ Day19.java
@@ -0,0 +1,10 @@
+ic class Main {
+	public static void main(String[] args) {
+		 String nama = "mawan ";
+		  String Nim = "D0224018";
+		   System.out.println("Nama:"+ nama );      
+    System.out.println("Nim:"+Nim);
+		 
+		
+	}
+}
