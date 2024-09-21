06459864258e7eda4e78ac4e3218374d03690ed0
Mawan2024 <mwan6703@gmail.com>
2024-09-21 09:37:33 +0800

    Hari 20
    Merampikan tipe data Reverens byte

---
+++ Day20.java
@@ -0,0 +1,11 @@
+package mawan1234;
+
+public class Mawan1234 {
+
+
+    public static void main(String[] args) {
+        Byte umur = 18;
+        System.out.println("umur" + umur+"tahun");
+    }
+    
+}
