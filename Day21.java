c16c0f747ee8b3d1521d032efd02c12224db0526
Mawan2024 <mwan6703@gmail.com>
2024-09-22 22:02:38 +0800

    Day21.java
    Tipe data Short

--- Day21.java
+++ Day21.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-		short maksimum = -32768;
-           short maksimal = 32767;
+		Short maksimum = -32768;
+           Short maksimal = 32767;
            System.out.println("nilai maksimum adala :"+ maksimum);
            System.out.println("nilai maksimal adala:"+ maksimal);
 	}
