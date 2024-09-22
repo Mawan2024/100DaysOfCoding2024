2ca7eac7b924cfa46ace45d395047bf726b5e502
Mawan2024 <mwan6703@gmail.com>
2024-09-22 10:09:41 +0800

    Hari 21
    Merampikan tipe data reverence short

---
+++ Day21.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		short maksimum = -32768;
+           short maksimal = 32767;
+           System.out.println("nilai maksimum adala :"+ maksimum);
+           System.out.println("nilai maksimal adala:"+ maksimal);
+	}
+}
