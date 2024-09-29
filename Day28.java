b312e61220262334e118a88b3e7e7883d015e221
Mawan2024 <mwan6703@gmail.com>
2024-09-29 21:13:45 +0800

    Update Day28.java
    Tipedata Boolean

--- Day28.java
+++ Day28.java
@@ -1,7 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
-	Boolean a = true;
-	Boolean b = false;
+	Boolean nilai1 = true;
+	Boolean nilai2 = false;
 	System.out.println("nilai a:"+a);
 	System.out.println("nilai b:"+b);
 	}
