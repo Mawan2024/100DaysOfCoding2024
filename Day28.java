0c4a6ad2aa02928295a5b4bbf0a642f545b397fb
Mawan2024 <mwan6703@gmail.com>
2024-09-29 21:15:12 +0800

    Day28.java
    Reverence bolean

--- Day28.java
+++ Day28.java
@@ -2,6 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 	Boolean nilai1 = true;
 	Boolean nilai2 = false;
-	System.out.println("nilai a:"+a);
-	System.out.println("nilai b:"+b);
+	System.out.println("nilai 1:"+nilai1);
+	System.out.println("nilai 2:"+nilai2);
 	}
