3c706579f48ad67533fd383a2ff9f64934b9d75a
Mawan2024 <mwan6703@gmail.com>
2024-10-01 00:54:07 +0800

    Hari 30
    Merampikan tipe data bigDecimal

---
+++ Day30.java
@@ -0,0 +1,8 @@
+import java.math.BigDecimal;
+
+public class Main {
+	public static void main(String[] args) {
+	BigDecimal angka1 =new BigDecimal("12345.789");
+	System.out.println("nilai bigDesimal:"+angka1);	
+	}
+}
