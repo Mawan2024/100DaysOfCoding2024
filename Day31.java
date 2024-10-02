bbef555a5aadf8a8cad965a4e26f50d792428eea
Mawan2024 <mwan6703@gmail.com>
2024-10-02 20:00:57 +0800

    Hari ke 31
    Mengkonversi string ke tipe data primitif

---
+++ Day31.java
@@ -0,0 +1,19 @@
+package mawan12334;
+
+public class MAWAN12334 {
+
+    
+    public static void main(String[] args) {
+        String a = "123";
+        int b = Integer.parseInt(a);
+        float c = Float.parseFloat(a);
+        double d = Double.parseDouble(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        
+        
+        
+    }
+    
+}
