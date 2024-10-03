0acc8a3287c320926b1bb52035c09b486d89b451
Mawan2024 <mwan6703@gmail.com>
2024-10-03 21:17:44 +0800

    Hari 32
    Mengkonversi tipe data ke string

---
+++ Day32.java
@@ -0,0 +1,29 @@
+// tipe data primitif 
+        byte a = 10;
+        short b = 100;
+        int c = 12000;
+        long d = 3000l;
+        float e = 24.6f;
+        double f = 23.78;
+        char g = 'A';
+        boolean h = true;
+        
+        // konversi ke string menggunakan to string 
+         String wan1 = Byte.toString(a);
+         String wan2 = Short.toString(b);
+         String wan3 = Integer.toString(c);
+         String wan4 = Long.toString(d);
+         String wan5 = Float.toString(e);
+         String wan6 = Double.toString(f);
+         String wan7 = Character.toString(g);
+         String wan8 = Boolean.toString(h);
+           
+        // output hasil konversi 
+         System.out.println("string dari Byte:"+ wan1);
+         System.out.println("string dari Short:"+ wan2);
+         System.out.println("string dari Integer:"+ wan3);
+         System.out.println("string dari Long:"+ wan4);
+         System.out.println("string dari Float:"+ wan5);
+         System.out.println("string dari Double:"+ wan6);
+         System.out.println("string dari Character:"+ wan7);
+         System.out.println("string dari Boolean:"+ wan8);
