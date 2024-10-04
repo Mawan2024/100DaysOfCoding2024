a3adf2c5b243b10c63491f850b0fc09d39819888
Mawan2024 <mwan6703@gmail.com>
2024-10-04 21:58:16 +0800

    Hari ke 33
    Merampikan tipe data casting

---
+++ Day33.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		// int ke double 
+		int a = 200;
+		double b = a;
+		System.out.println("nilai double " + b);
+		
+		// double ke int 
+		int c = (int) b;
+		System.out.println("nilai int" + c);
+	}
+}
