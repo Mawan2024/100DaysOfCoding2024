e87c5093089c0ed031eeb5f87bb11b0d3a20190d
Mawan2024 <mwan6703@gmail.com>
2024-10-05 17:18:52 +0800

    Hari 34
    Merampikan tipe data input pada keyboard

---
+++ Day34.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner kata = new Scanner (System.in);
+		System.out.print("masukan nama :");
+		String nama = kata.nextLine();
+		System.out.print("masukan umur :");
+		int umur = kata.nextInt();
+		System.out.print ("masikan berat badan : ");
+		double beratbadan = kata.nextDouble();
+		
+	}
+}
