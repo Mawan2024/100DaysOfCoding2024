ace601753d4f273319eab84fe7294c20a8e2aca4
Mawan2024 <mwan6703@gmail.com>
2024-10-06 20:11:18 +0800

    Hari 35
    Merampikan tipe data operator penjumlahan dan pengurangan

---
+++ Day35.java
@@ -0,0 +1,15 @@
+ublic class Main {
+	public static void main(String[] args) {
+		
+		//penjumlahan 
+		int  nilai = 20;
+		 double angka = 23;
+		 System.out.println("penjumlahan:");
+		 System.out.println("hasil penjumlahan: "+(nilai+angka ) );
+		 
+	// pengurangan
+	   int  a = 20;
+	    double  b = 10;
+	    System.out.println("pengurangan :");
+	     System.out.println("hasil pengurangan :" +(a-b));
+	}
