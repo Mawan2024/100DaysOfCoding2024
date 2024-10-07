7a8ee86ec64e2047c5ab439326874217bacea57c
Mawan2024 <mwan6703@gmail.com>
2024-10-07 20:15:49 +0800

    Hari ke 36
    Merampikan operator pembagian dan perkalian

---
+++ Day36.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		 // operator perkalian
+		 int a = 8;
+		int b = 4;
+		System.out.println("operator perkalian:");
+		System.out.println ("perkalian:" + (a*b));
+		
+		//pembagian
+		 double nilai = 8;
+		 double angka = 2;
+		 System.out.println("pembagian :");
+		 System.out.println("hasil pembagian:" + (nilai /angka));
+		
+	}
+}
