f9182c07416e2e777c46c954cf84e8d534690e41
Mawan2024 <mwan6703@gmail.com>
2024-10-07 21:11:57 +0800

    Day36.java
    Operator perkalian ,pembagian dan modolu

--- Day36.java
+++ Day36.java
@@ -1,17 +1,11 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		 // operator perkalian
-		 int a = 8;
+		int a = 8;
 		int b = 4;
-		System.out.println("operator perkalian:");
-		System.out.println ("perkalian:" + (a*b));
-		
-		//pembagian
-		 double nilai = 8;
-		 double angka = 2;
-		 System.out.println("pembagian :");
-		 System.out.println("hasil pembagian:" + (nilai /angka));
-		
+		int c =  a*b;
+		int d = a/b;
+		int e = a%b;
+		 System.out.println("Perkalian : "+ c  + "\n pembagian :" + d   +"\n modolu : " +  e);
 	}
 }
