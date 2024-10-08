7b22e290ca06f93d05fa835943e09dc4c8dc91f8
Mawan2024 <mwan6703@gmail.com>
2024-10-08 18:05:55 +0800

    Hari ke 37
    Merampikan operator penugasan: pengisian nilai pengisian dan penambahan pengisian dan pengurangan

---
+++ Day37.java
@@ -0,0 +1,9 @@
+public static void main(String[] args) {
+       // pengisian nilai
+       int nilai; // deklarasi variabel
+       int x = 10; // pengisian nilai
+       System.out.println("nilai x :" + x); // output nilai x:10
+       x += 5;  // penambahan  nilai (x= x + 5)
+        System.out.println("setelah penambahan:" + x); // output : setela penambahan :15
+       x -= 3 ; // penngurangan nilai (x = x -3)
+        System.out.println("setelah pengurangan:" + x); // output : setelah pengurangan : 7
