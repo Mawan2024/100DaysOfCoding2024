125e5dab81f6d40cc258fccdae6b5eed04ab6682
Mawan2024 <mwan6703@gmail.com>
2024-10-09 18:37:30 +0800

    Hari ke 38
    Merampikan operator penugasan pengisian perkalian dan pengisian pembagian

---
+++ Day38.java
@@ -0,0 +1,15 @@
+package mawan;
+
+public class Mawan {
+
+    public static void main(String[] args) {
+        
+       int x = 10; // pengisian nilai
+       System.out.println("nilai x :" + x); // output nilai x:10 
+       x *= 5;  // perkalian  nilai (x= x * 5)
+        System.out.println("setelah perkalian:" + x); // output : setela perkalian :50;
+       x /= 5 ; // pembagian nilai (x = x /5)
+        System.out.println("setelah pembagian:" + x); // output : setelah pembagian : 10
+    }
+    
+}
