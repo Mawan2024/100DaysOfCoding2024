95c9b3899ff32b00ef2b876af446618d72a3bc4e
Mawan2024 <mwan6703@gmail.com>
2024-10-11 10:44:21 +0800

    Hari ke 40
    Merampikan operator penugasan ,lebih kecil dan lebih besar

---
+++ Day40.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		if(a<b) // nilai a lebih kecil dari nilai b
+		if(b>a) // nilai b lebih besar dari nilai a
+		 System.out.println(a +"lebih kecil" +  b);
+		 System.out.println(b + "lebih besar" +  a);
+	}
+}
