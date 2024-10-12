ff27ad151db34941590d7b8d121388da4270cf73
Mawan2024 <mwan6703@gmail.com>
2024-10-12 22:13:27 +0800

    Hari ke 41
    Soal evaluasi yaitu mengkonversi tipe data string  ke int

---
+++ Day41.java
@@ -0,0 +1,10 @@
+import java .util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner nilai= new Scanner (System.in);
+			String tinggibadan = nilai.nextLine();
+			int tinggi = Integer.parseInt(tinggibadan );
+			System.out.println("tinggi badan  :"+tinggibadan+"cm");
+			
+	}
+}
