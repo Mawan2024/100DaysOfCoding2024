289a05785d4e78ecfaa84ccc15a3a8a5ea2f1747
Mawan2024 <mwan6703@gmail.com>
2024-10-13 19:49:36 +0800

    Hari ke 42
    Merampikan operator penugasan tidak sama dengan dan sama dengan

---
+++ Day42.java
@@ -0,0 +1,17 @@
+package mawan123;
+
+public class Mawan123 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        boolean c =(a != b);//  a tidak sama dengan  b
+        System.out.println("variabel a tidak sama dengan variabel b:" );
+        
+        // variabel sama 
+        int nilai = 4;
+        int angka = 4;
+        boolean angkadannilai = (nilai==angka);// nilai sama dengan angka
+        System.out.println("variabel nilai sama dengan variabel angka:");
+    } 
+    
+}
