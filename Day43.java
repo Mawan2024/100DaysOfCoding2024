20db5a7246d85521b568d6de29b4128b94887673
Mawan2024 <mwan6703@gmail.com>
2024-10-14 18:06:22 +0800

    Hari ke 43
    Merampikan operator  lebih kecil sama dengan dan lebih besar sama dengan

---
+++ Day43.java
@@ -0,0 +1,19 @@
+package day43.java;
+
+
+public class Day43Java {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        boolean a_dan_b = (a<=b);// a lebi kecil sama dengan b;
+        System.out.println("variabel a lebi kecil sama dengan b:" +a_dan_b);
+        
+        // lebih besar sama dengan 
+        int c = 10;
+        int d = 2;
+        boolean c_dan_d = (c>=d); // c lebih besar sama dengan d
+        System.out.println("variabel c lebi besar sama dengan variabel d:"+ c_dan_d);
+        
+    }
+    
+}
