b70b7ca706e85654581ed56c972d0f393078d5c5
Mawan2024 <mwan6703@gmail.com>
2024-10-15 20:08:24 +0800

    Hari ke 44
    Merampikan operator logika AND

---
+++ Day44.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = true ;
+		 // operator logika AND
+		  boolean hasil = a && b ;
+		 System.out.println("hasil dari a && b adala :" + hasil);
+	}
+}
