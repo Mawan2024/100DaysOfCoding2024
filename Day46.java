e4adf354bb9c94f7cd24f36b022be99980fab087
Mawan2024 <mwan6703@gmail.com>
2024-10-17 21:37:24 +0800

    Hari ke 46
    Merampikan operator logika negasi atau kebalikan

---
+++ Day46.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		boolean nilai = true;
+		boolean hasil = ! nilai;//operator negasi
+		System.out.println("nilai awal:" + nilai);       System.out.println("hasil negasi :" + hasil);
+		
+	}
+}
