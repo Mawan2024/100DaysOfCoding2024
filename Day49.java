7c4cf6ba106a63073b77e4af1e255596ebf60b5a
Mawan2024 <mwan6703@gmail.com>
2024-10-20 19:17:04 +0800

    Hari ke 49
    Merampikan operator percabangan if else if

---
+++ Day49.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner input  = new Scanner(System.in);
+		System.out.println("masukan nilai:");
+		int nilai = input. nextInt();
+		if (nilai>1){
+		System.out.print("nilai lebih besar dari 1:");
+	}	else if ( nilai< 10){
+		System.out.print("nilai lebi kecil dari 10 :");
+		
+	}else{
+		
+}
+
+}
+}
