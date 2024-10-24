a1de09935a635c2aa5952454068acffc81ac7895
Mawan2024 <mwan6703@gmail.com>
2024-10-24 19:24:06 +0800

    Hari ke 53
    Merampikan operator looping for

---
+++ Day53.java
@@ -0,0 +1,12 @@
+package day53.java;
+
+
+public class Day53Java {
+
+    public static void main(String[] args) {
+        for(int a=1; a<10; a++){
+        System.out.println(a);
+    }
+    
+}
+}
