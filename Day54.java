9e90dabf8f6235abfff3c273c4009079971387a8
Mawan2024 <mwan6703@gmail.com>
2024-10-25 20:51:42 +0800

    Hari ke 54
    Merampikan operator for inchrament

---
+++ Day54.java
@@ -0,0 +1,12 @@
+package day54.java;
+
+
+public class forincrament {
+    public static void main(String[] args) {
+        int n =10;
+        for(int a=1; a<n; a++){
+            System.out.println("nilai a:"+a);
+        }
+    }
+    
+}
