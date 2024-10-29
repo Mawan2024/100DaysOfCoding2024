f81c91351c71d5eebcc2f8971ec62542deec16bb
Mawan2024 <mwan6703@gmail.com>
2024-10-29 19:01:29 +0800

    Hari ke 58
    Merampikan looping while decrament

---
+++ Day58.java
@@ -0,0 +1,9 @@
+public class whilein {
+    public static void main(String[] args) {
+        int a= 0; 
+        while (a< 10) { 
+            System.out.println(i); 
+            a--;
+        }
+    }
+}
