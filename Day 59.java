08ee775e384849960aa3522605d6928b390a95e7
Mawan2024 <mwan6703@gmail.com>
2024-10-30 22:32:02 +0800

    Hari ke 59
    Merampikan operator for each

---
+++ Day 59.java
@@ -0,0 +1,12 @@
+package DAY59.JAVA;
+
+
+public class latihan_soal {
+    public static void main(String[] args) {
+        int[] angka ={1,2,3,4,5};
+        for(int num : angka){
+            System.out.println("angka:"+num);
+        }
+    }
+    
+}
