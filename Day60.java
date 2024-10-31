fcaf814ea146b9488564ea0c01f2db4bf87c69e1
Mawan2024 <mwan6703@gmail.com>
2024-10-31 09:55:53 +0800

    Hari 60
    Merampikan looping do while

---
+++ Day60.java
@@ -0,0 +1,15 @@
+package day60.java;
+
+
+public class loopingdowhile {
+    public static void main(String[] args) {
+        int a = 1;
+        do {
+            System.out.println("angka:"+a);
+            a++;     
+    }while (a<=10);{
+        
+    }
+    }
+    
+}
