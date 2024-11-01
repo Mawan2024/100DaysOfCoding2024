533e3fad0053163af4316c04c7a1f864edf675c3
Mawan2024 <mwan6703@gmail.com>
2024-11-01 19:04:19 +0800

    Hari ke 61
    Merampikan infinete loop

---
+++ Day61.java
@@ -0,0 +1,11 @@
+package day61.java;
+
+
+public class infinite_loop {
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("loop tanpa henti-for");
+        }
+            }
+    
+}
