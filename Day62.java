43f5ce7fa189e5f00a07f036e1d53501104dde38
Mawan2024 <mwan6703@gmail.com>
2024-11-02 19:18:09 +0800

    Create Day62.java
    Keyword break

---
+++ Day62.java
@@ -0,0 +1,18 @@
+package day62.java;
+public class keyword_break {
+    public static void main(String[] args) {
+        for(int a = 0; a<=10; a++){
+           if(a==5){
+               break;
+               
+            
+           }
+            System.out.println("nilai:"+ a);
+           
+           
+            System.out.println("loop berhenti karena adanya break");
+        }
+            
+    }
+    
+}
