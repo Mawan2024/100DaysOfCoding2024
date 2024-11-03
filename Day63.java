a837f2c3e78e08c68dd04cf321b4fc2c699ab83f
Mawan2024 <mwan6703@gmail.com>
2024-11-03 18:55:15 +0800

    Hari ke 63
    Erampikan keyword continue

---
+++ Day63.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+	for(int a= 1; a<=10; a++){
+	      if(a%2==0){
+	            continue  ;
+	      }
+	     System.out.print(a);	
+	
+}
+	}
+}
