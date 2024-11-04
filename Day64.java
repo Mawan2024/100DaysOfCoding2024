7cc9cee8473c74a0caa2de20f3a73d3d7e8e247e
Mawan2024 <mwan6703@gmail.com>
2024-11-04 20:53:52 +0800

    Hari 64
    Break label

---
+++ Day64.java
@@ -0,0 +1,20 @@
+package days64;
+
+public class days64 {
+    
+    public static void main(String[] args) {
+        
+        Mawan:
+        for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 5; j++) {
+                System.out.println("i = " + i + ", j = " + j);
+
+ if (i == 2 && j == 5) {
+    break Mawan;
+                }
+            }
+        }
+        
+        System.out.println("Loop selesai.");
+    }
+}
