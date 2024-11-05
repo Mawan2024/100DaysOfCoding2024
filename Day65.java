129e0dd98e56423cc69e19d4877679610d09c525
Mawan2024 <mwan6703@gmail.com>
2024-11-05 20:13:33 +0800

    Hari ke 65
    Nested loop

---
+++ Day65.java
@@ -0,0 +1,12 @@
+public class nesdet_loop {
+    public static void main(String[] args) {
+        int mawan = 10;
+        for(int a = 1; a<=mawan; a++){
+            for(int b = 1; b<=a; b++){
+                System.out.print("*");
+            }
+            System.out.println(" ");    
+        }
+    }
+    
+}
