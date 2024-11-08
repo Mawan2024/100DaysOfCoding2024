037d3a673fe4347ff28eed78cd518768c317ceb0
Mawan2024 <mwan6703@gmail.com>
2024-11-08 21:14:25 +0800

    Hari 68
    Merampikan non void

---
+++ Day68.java
@@ -0,0 +1,12 @@
+public class CONTONO_VOID {
+    public int kalidua(int angka){
+        
+        
+            return angka*2;
+            
+        }
+    public static void main(String[] args) {
+            CONTONO_VOID CONTO = new CONTONO_VOID();
+                System.out.println("hasilnya;"+ CONTO.kalidua(5));
+    }
+    }
