6bf76e66cf434ab23f787dde95ccd1ebbf9f6b06
Mawan2024 <mwan6703@gmail.com>
2024-11-09 22:33:53 +0800

    Hari ke 69
    Soal evaluasi

---
+++ Day.69.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+
+       for(int a= 1; a<=10; a++){
+               if(a == 5){
+                    break;
+                }
+                for(int b = 1; b <=3; b++){
+                 System.out.print("* ");
+                }
+//
+//                
+           }	
+	}
+}
