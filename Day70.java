623329f49ee703e8e6ca49cdc045f2b8036f69e3
Mawan2024 <mwan6703@gmail.com>
2024-11-10 21:28:38 +0800

    Hari ke 70
    Merampikan method parameter

---
+++ Day70.java
@@ -0,0 +1,12 @@
+public class methodparameter {
+    public int perkalian(int a, int b){
+        return a*b;
+    }
+            public static void main(String[] args) {
+        methodparameter conto = new methodparameter();
+        int hasil = conto.perkalian(4,3);
+                System.out.println("hasil perkalian:"+hasil);;
+        
+    }
+    
+}
