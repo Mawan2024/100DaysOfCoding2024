525ccb3c8fdcd3b4b572b811c29660f310936185
Mawan2024 <mwan6703@gmail.com>
2024-11-15 16:24:32 +0800

    Create Day75.java
    Merampikan string method tolowercase

---
+++ Day75.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+public class Day75 {
+    public static void main(String[] args) {
+        // String Method : toLowerCase()
+        
+        String nama = "MAWAN";
+        String lowerText = nama.toLowerCase();
+        System.out.println(lowerText);
+    }
+}
