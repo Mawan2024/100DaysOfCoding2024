ac08f31682ee1949feb0e6218e5ac0e14fd5e47a
Mawan2024 <mwan6703@gmail.com>
2024-11-16 17:17:16 +0800

    Create Day76.java
    Merampikan string method uppercase

---
+++ Day76.java
@@ -0,0 +1,11 @@
+package project100dayscoding;
+
+
+public class DAY76 {
+    public static void main(String[] args) {
+        String teks = "halo sobad";
+        String UpperCaseteks = teks.toUpperCase();
+        System.out.println("hasil dalam huruf kapital:"+UpperCaseteks);
+    }
+    
+}
