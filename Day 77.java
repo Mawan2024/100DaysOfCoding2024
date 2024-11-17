0d9cba815520b307ae132adaac2dab6acbd6e6f5
Mawan2024 <mwan6703@gmail.com>
2024-11-17 21:18:25 +0800

    Create Day 77.java
    Merampikan string method equals

---
+++ Day 77.java
@@ -0,0 +1,17 @@
+package project100dayscoding;
+
+public class day77 {
+    public static void main(String[] args) {
+        String kata1 ="Mawan";
+        String kata2 ="Mawan";
+        String kata3 = "mawan";
+        
+        boolean nilaisama = kata1.equals(kata2);
+        boolean nilaitidaksama = kata1.equals(kata3);
+        
+           System.out.println("apaka kata 1 sama dengan kata 2:"+ nilaisama);
+           System.out.println("apaka kata 1 tidak sama dengan kata 3:"+nilaitidaksama);
+    
+             
+ }    
+ }
