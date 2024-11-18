36fec537408e23f5d546478e048ca5e54437a6d3
Mawan2024 <mwan6703@gmail.com>
2024-11-18 20:57:45 +0800

    Create Day78.java
    Merampikan string method equalsnorecase

---
+++ Day78.java
@@ -0,0 +1,12 @@
+public class day78 {
+    public static void main(String[] args) {
+        String a = "mawan";
+        String b = "MAWAN";
+        if(a.equalsIgnoreCase(b)){
+            System.out.println("sama");
+        }else{
+            System.out.println("tidak sama");
+        }
+    }
+    
+}
