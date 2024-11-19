ad10bb90176d8fd1c7109d447dbfa0d427886de4
Mawan2024 <mwan6703@gmail.com>
2024-11-20 00:00:31 +0800

    Hari ke 79

--- Day79.java
+++ Day79.java
@@ -1,20 +1,7 @@
-class mahasiswa {
-    
-        private String nama;
-        private int umur;
-        public mahasiswa(String nama, int umur){
-            this.nama = nama;
-            this.umur = umur;
-        }
-        @Override
-        public String toString(){
-            return "nama:"+nama+",umur:"+umur+"tahun";
-}
-}
-public class main {
-    public static void main(String[] args) {
-       mahasiswa mhs = new mahasiswa("mawan", 19);
-        
-        System.out.println(mhs);
-    }
+
+public class Main {
+	public static void main(String[] args) {
+	string nama = "mawan";
+	System.out.println(nama.toString());	
+	}
 }
