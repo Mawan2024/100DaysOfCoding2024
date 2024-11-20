d6ca90f5451bfc8eb387ec50ab18f333c950f42c
Mawan2024 <mwan6703@gmail.com>
2024-11-20 22:11:59 +0800

    Create Day80.java
    String method trim

---
+++ Day80.java
@@ -0,0 +1,16 @@
+public class day80 {
+    public static void main(String[] args) {
+       
+   
+        String nama = "   marwansya   ";
+        
+        // Menampilkan string sebelum trim
+        System.out.println("String sebelum trim: \"" +nama + "\"");
+        
+        // Menggunakan metode trim()
+        String trimmedString = nama.trim();
+        
+        // Menampilkan string setelah trim
+        System.out.println("String setelah trim: \"" + trimmedString + "\"");
+    }
+}
