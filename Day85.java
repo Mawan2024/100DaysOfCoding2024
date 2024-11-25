fe64f067733c61bc8f5615312ba641b5a22e7e6a
Mawan2024 <mwan6703@gmail.com>
2024-11-25 22:41:07 +0800

    Create Day85.java
    Mengisi element ke array satu dimensi pada saat di inisialisasi

---
+++ Day85.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {                        
+	int [] numbers = {10,20,30,40,50};
+	System.out.println("isi array");
+	for(int i = 0; i<numbers.length;  i++){
+	      System.out.println("element ke"+ i+ ":"+numbers[i]);	
+	}
+}
+}
