c442262c0619960771bde91bc108ed572e0fe603
Mawan2024 <mwan6703@gmail.com>
2024-11-26 20:25:34 +0800

    Create Day86.java
    Mencetak  elemen pada array dalam satu dimensi

---
+++ Day86.java
@@ -0,0 +1,11 @@
+public class DAY86 {
+   
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        // Mencetak elemen array
+        for (int elemen : array) {
+            System.out.println(elemen);
+        }
+    }
+}
