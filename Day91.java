1388a5962c3f8173a2455790b057f7548f9cc84f
Mawan2024 <mwan6703@gmail.com>
2024-12-01 20:13:56 +0800

    Create Day91.java
    Soal percabangan  you and I

---
+++ Day91.java
@@ -0,0 +1,25 @@
+package day91;
+
+import java.util.Scanner;
+
+public class Day91 {
+
+ 
+    public static void main(String[] args) {
+        Scanner input  = new Scanner(System.in);
+        while(true){
+            System.out.println("masukan sebuah bilangan");
+        
+        int angka =input.nextInt();
+        if(angka%2==0){
+            System.out.println("genap");
+        }else{
+            System.out.println("you and I and");
+        }
+       
+        
+        
+    }
+    
+}
+}
