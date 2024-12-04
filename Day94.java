235a077a11904a77780d1bd492dbad7e4668bf41
Mawan2024 <mwan6703@gmail.com>
2024-12-04 22:04:46 +0800

    Create Day94.java
    Membuat pola segitiga

---
+++ Day94.java
@@ -0,0 +1,15 @@
+public class SegitigaSamaKaki {
+    public static void main(String[] args) {
+        int tinggi = 5; // Tinggi segitiga
+
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = tinggi - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
